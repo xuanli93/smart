cdcbbec0f569ebddc6a6c584b65aa75f3ff6a255
Xuan Li <xuanl@retarus.com>
2019-08-15 17:32:53 -0400

    mysecond

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
-from flask import flask
+import flask
 
-app  = Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello/<name>')
+
+def hello_world(name):
+   return 'Hello World %s' %name
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
+	app.run(debug = True)
