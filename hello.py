71111a0e807b3e5984dec6cbccf3835079397803
Xuan Li <xuanl@retarus.com>
2019-08-15 17:59:50 -0400

    3rd

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
-import flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
+@app.route('/admin')
+def admin():
+	return "admin page"
 
-def hello_world(name):
-   return 'Hello World %s' %name
+@app.route('/guest')
+def guest():
+	return "guest page"
+
+@app.route('/user/<name>')
+def user(name):
+	if name=='admin':
+		return redirect(url_for('admin'))
+	else:
+		return redirect(url_for('guest', guest = name))
+
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
 	app.run(debug = True)
